944a083fcd98c49871f797063be04824b86fa71b
ddiazsouto <ddiazsouto@gmail.com>
2021-11-18 15:40:59 +0000

    Automated environment setup

---
+++ setup_vm.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+# Installing Docker and docker compose
+
+echo "----fetching Docker----"
+sudo apt-get update
+sudo apt install curl -y
+curl https://get.docker.com | sudo bash
+
+echo "----Installing Docker-compose------"
+# make sure jq & curl is installed
+sudo apt update
+sudo apt install -y curl jq
+# set which version to download (latest)
+version=$(curl -s https://api.github.com/repos/docker/compose/releases/latest | jq -r '.tag_name')
+# download to /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/${version}/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+# make the file executable
+sudo chmod +x /usr/local/bin/docker-compose
+
+
+# Now Jenkins
+
+echo "Time for Jenkins to come"
+
+if type apt > /dev/null; then
+    pkg_mgr=apt
+    java="openjdk-8-jre"
+elif type yum /dev/null; then
+    pkg_mgr=yum
+    java="java"
+fi
+echo "updating and installing dependencies"
+sudo ${pkg_mgr} update
+sudo ${pkg_mgr} install -y ${java} wget git > /dev/null
+echo "configuring jenkins user"
+sudo useradd -m -s /bin/bash jenkins
+echo "downloading latest jenkins WAR"
+sudo su - jenkins -c "curl -L https://updates.jenkins-ci.org/latest/jenkins.war --output jenkins.war"
+echo "setting up jenkins service"
+sudo tee /etc/systemd/system/jenkins.service << EOF > /dev/null
+[Unit]
+Description=Jenkins Server
+
+[Service]
+User=jenkins
+WorkingDirectory=/home/jenkins
+ExecStart=/usr/bin/java -jar /home/jenkins/jenkins.war
+
+[Install]
+WantedBy=multi-user.target
+EOF
+sudo systemctl daemon-reload
+sudo systemctl enable jenkins
+sudo systemctl restart jenkins
+sudo su - jenkins << EOF
+until [ -f .jenkins/secrets/initialAdminPassword ]; do
+    sleep 1
+    echo "waiting for initial admin password"
+done
+until [[ -n "\$(cat  .jenkins/secrets/initialAdminPassword)" ]]; do
+    sleep 1
+    echo "waiting for initial admin password"
+done
+echo "initial admin password: \$(cat .jenkins/secrets/initialAdminPassword)"
+EOF
