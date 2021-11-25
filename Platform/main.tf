e851c14eb7582375d086c44aceb5cf1c103d8653
ddiazsouto <ddiazsouto@gmail.com>
2021-11-25 17:23:43 +0000

    Improved terrafomr.tf

--- Platform/main.tf
+++ Platform/main.tf
@@ -56,30 +56,35 @@ resource "azurerm_public_ip" "myterraformpublicip" {
     }
 }
 
-# Create Network Security Group and rule
 
-resource "azurerm_network_security_group" "myterraformnsg" {
-    name                = "SG"
-    location            = "eastus"
-    resource_group_name = azurerm_resource_group.myterraformgroup.name
-
-    security_rule {
-        name                       = "SSH"
-        priority                   = 1001
-        direction                  = "Inbound"
-        access                     = "Allow"
-        protocol                   = "Tcp"
-        source_port_range          = "*"
-        destination_port_range     = "22"
-        source_address_prefix      = "*"
-        destination_address_prefix = "*"
-    }
-
-    tags = {
-        environment = "Scraper"
-    }
+resource "azurerm_virtual_network" "main" {
+  name                = "${var.prefix}-network"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.myterraformgroup.location
+  resource_group_name = azurerm_resource_group.myterraformgroup.name
 }
 
+resource "azurerm_subnet" "internal" {
+  name                 = "internal"
+  resource_group_name  = azurerm_resource_group.myterraformgroup.name
+  virtual_network_name = azurerm_virtual_network.main.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
+
+
+
+
+resource "azurerm_network_interface" "myterraformnic" {
+  name                = "mynic"
+  location            = azurerm_resource_group.myterraformgroup.location
+  resource_group_name = azurerm_resource_group.myterraformgroup.name
+
+  ip_configuration {
+    name                          = "testconfiguration1"
+    subnet_id                     = azurerm_subnet.internal.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
 
 
 # Create (and display) an SSH key
@@ -97,6 +102,7 @@ resource "azurerm_linux_virtual_machine" "myterraformvm" {
     name                  = "myVM"
     location              = "eastus"
     resource_group_name   = azurerm_resource_group.myterraformgroup.name    
+    network_interface_ids = [azurerm_network_interface.myterraformnic.id]
     size                  = "Standard_B1s"
 
 
