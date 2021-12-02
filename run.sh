16557d42b88d2ce5fbae53a3a9ff5a58f35c2881
ddiazsouto <ddiazsouto@gmail.com>
2021-12-02 16:11:05 +0000

    added a docker compose

--- run.sh
+++ run.sh
@@ -8,7 +8,7 @@ sudo chmod 400 llave
 ip=$(terraform output | grep public_IP_grab | awk '{print $3}')
 workers=$(terraform output | grep 10.0 | awk '$2 {print $3}' | uniq)
 
-# First three workers local IP, is better to use $workers as it makes its 
+# First three workers local IP, is better to use variable $workers as it makes its 
 # posterior interaction with python more scalable
 
 worker1=$(echo $workers | cut -f1 -d" ")
