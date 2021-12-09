3046a19fd6e6a41ed483d1bea85357f41ea52559
ddiazsouto <ddiazsouto@gmail.com>
2021-12-09 16:46:04 +0000

    update

--- frontend_master/app.py
+++ frontend_master/app.py
@@ -11,11 +11,13 @@ app = Flask(__name__)
 def hello_world():
 
 
-    x = requests.get('http://10.0.2.15:5500/').text #.json()
-    y = requests.get('http://10.0.2.15:5000/').text #.json()
+    #x = requests.get('http://10.0.2.15:5500/').text #.json()
+    #y = requests.get('http://10.0.2.15:5000/').text #.json()
 
+    with open('IPs.txt', 'r') as f:
+        var = f.readlines()
 
-    return str(x + '\n' + y)
+    return str('var')
 
 
 if __name__ == '__main__':
