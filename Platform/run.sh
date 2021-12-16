7ec2132e343f1dd60a37a0e68eba808ccdabdb85
ddiazsouto <ddiazsouto@gmail.com>
2021-12-16 10:08:00 +0000

    creates an inventory file for master

--- Platform/run.sh
+++ Platform/run.sh
@@ -21,4 +21,9 @@ cd configuration
 echo [master] > inventory.yml
 echo "master@$ip     ansible_ssh_private_key_file=~/Microservices_Housing/Platform/llave ">> inventory.yml
 
+echo [workers] > master_conf/inventory.yml
+echo "worker@$worker1" >> master_conf/inventory.yml
+echo "worker@$worker2" >> master_conf/inventory.yml
+echo "worker@$worker3" >> master_conf/inventory.yml
+
 ansible-playbook -i inventory.yml playbook.yml
